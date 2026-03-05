abed4efb4a61a147ea50bb6df63189ddb704bb4d
yashaswinimullalli <yashaswinimullalli4@gmail.com>
2026-03-05 19:15:47 +0530

    update changes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById('hospitalBtn').onclick = () => {
     window.open(DEPLOYMENT_URLS.hospital, '_blank'); 
 };
 
-// Optional: Navbar Scroll Effect
+// Navbar Scroll Effect
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('.navbar');
     if (window.scrollY > 50) {
